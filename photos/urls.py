dba5ed8d71af40e9ed03ac5f43835ce56579f2b3
Okall Vivian <vivieokall@gmail.com>
2019-03-03 23:52:02 +0300

    Update url and settings

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
-     url(r'^search/', views.search_results, name='search_results')
+     url(r'^search/', views.search_results, name='search_results'),
+
     # url(r'^photo/$', views.about, name='about'),
     # url(r'^(?P<topic_name>\w+)/(?P<photo_id>[0-9]+)/$', views.info, name='info')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
