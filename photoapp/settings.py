c8328895e1bc9a4291c4bcea7059852aab8cf16d
Okall Vivian <vivieokall@gmail.com>
2019-03-04 18:02:53 +0300

    "Modify files"

--- photoapp/settings.py
+++ photoapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'photos'
+    'bootstrap3'
     'photos.apps.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
